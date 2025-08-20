a05eea5ad472220316be31476d3b2773292f3400
404NorADev <imanalievdanielsamarovich@gmail.com>
2025-08-20 19:26:35 +0600

    исправление ошибок

--- payments/models.py
+++ payments/models.py
@@ -12,7 +12,6 @@ class Payment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     booking = models.ForeignKey(Booking, on_delete=models.CASCADE, related_name='payments')
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    stripe_payment_intent = models.CharField(max_length=255, unique=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="pending")
     created_at = models.DateTimeField(auto_now_add=True)
 
