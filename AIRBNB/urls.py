9364b67bcb1c7926852928a2ab5bd699e3b5ee2e
alipy-dw <ebaka646@gmail.com>
2025-08-22 23:30:33 +0600

    недоделанный сваггер

--- AIRBNB/urls.py
+++ AIRBNB/urls.py
@@ -15,9 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.urls import path, include, re_path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
 from django.conf import settings
 from django.conf.urls.static import static
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API",
+        default_version='v1',
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+)
 
 
 urlpatterns = [
@@ -28,7 +41,11 @@ urlpatterns = [
     path('api/payments/', include('payments.urls')),
     path('api/reviews/', include('reviews.urls')),
     path('api/favorites/', include('favorites.urls')),
+    # path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    re_path(r'^docs/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
+
+# urlpatterns += [path('docs/', schema_view.with_ui('swagger'))]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
